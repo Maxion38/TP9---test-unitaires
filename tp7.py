5b9e55dedffcc0ccea98c8f174dc5db774d15cb3
Maxion38 <147092829+Maxion38@users.noreply.github.com>
2023-12-06 20:04:17 +0100

    Spécifications update.py

--- tp7.py
+++ tp7.py
@@ -1,6 +1,8 @@
 # Exceptions
 class NotANumberException(Exception):
     pass
+
+
 class NotFractionInstanceException(Exception):
     pass
 
@@ -13,11 +15,12 @@ class Fraction:
     This class allows fraction manipulations through several operations.
     """
 
-    def __init__(self, num=0, den=1):
+    def __init__(self, num=0.0, den=1.0):
         """This builds a fraction based on some numerator and denominator.
 
         PRE : No specific preconditions
-        POST : Simplified num and simplified den represent the reduced fraction form
+        POST : __num and __den are the fraction elements in the reduced form
+               __initial_num and __initial_den are the fraction elements as the user input
         RAISES : NotANumberException if num or den are not numbers (float or int)
                  ZeroDivisionError if den == 0
         """
@@ -26,40 +29,45 @@ class Fraction:
             raise NotANumberException("Fraction must be composed of numbers")
 
         if den == 0:
-            raise ZeroDivisionError
+            raise ZeroDivisionError("Impossible to divide by zero")
 
-        self.__num = num
-        self.__den = den
+        self.__initial_num = num
+        self.__initial_den = den
 
         pgcd = self.__calc_pgcd()
         if pgcd == "numZero":
-            self.__simplified_num = 0
-            self.__simplified_den = 1
+            self.__num = 0
+            self.__den = 1
         else:
-            sim_num = int(num / pgcd)
-            sim_den = int(den / pgcd)
-            if sim_den < 0:
-                sim_num = 0 - sim_num
-                sim_den = abs(sim_den)
+            simplified_num = int(num / pgcd)
+            simplified_den = int(den / pgcd)
+            if simplified_den < 0:
+                simplified_num = 0 - simplified_num
+                simplified_den = abs(simplified_den)
 
-            self.__simplified_num = sim_num
-            self.__simplified_den = sim_den
-
-    @property
-    def num(self):
-        return self.__simplified_num
+            self.__num = simplified_num
+            self.__den = simplified_den
 
     @property
-    def den(self):
-        return self.__simplified_den
+    def get_num(self):
+        return self.__num
+
+    @property
+    def get_den(self):
+        return self.__den
 
     def __calc_pgcd(self):
-        if self.__num < self.__den:
-            small_num = self.__num
-            big_num = self.__den
+        """Return the pgcd of the Fraction
+
+        PRE : No specific preconditions
+        POST : Returns the pgcd as a number or if self.__initial_num == 0 returns "numZero"
+        """
+        if self.__initial_num < self.__initial_den:
+            small_num = self.__initial_num
+            big_num = self.__initial_den
         else:
-            small_num = self.__den
-            big_num = self.__num
+            small_num = self.__initial_den
+            big_num = self.__initial_num
 
         rest = -1
         if small_num == 0:
@@ -73,13 +81,14 @@ class Fraction:
 
     # ------------------ Textual representations ------------------
 
+
     def __str__(self):
         """Return a textual representation of the reduced form of the fraction
 
         PRE : No specific preconditions
         POST : Returns a str of the simplified num and den
         """
-        return str(self.__simplified_num) + "/" + str(self.__simplified_den)
+        return str(self.__num) + "/" + str(self.__den)
 
 
     def as_mixed_number(self):
@@ -91,15 +100,15 @@ class Fraction:
         POST : Returns a str of the mixed fraction
         """
 
-        den = self.__simplified_den
-        num = self.__simplified_num
+        den = self.__den
+        num = self.__num
         whole = num // den
         rest = num % den
-        if whole == 0 :
-            proper_fraction = str(num) + "/" +str(den)
+        if whole == 0:
+            proper_fraction = str(num) + "/" + str(den)
 
         else:
-            if rest > 0 :
+            if rest > 0:
                 proper_fraction = str(whole) + " " + str(rest) + "/" + str(den)
             else:
                 proper_fraction = str(whole)
@@ -119,10 +128,10 @@ class Fraction:
         if not(isinstance(other, Fraction)):
             raise NotFractionInstanceException("The operation must be between two Fraction class instances")
 
-        num = self.__simplified_num
-        den = self.__simplified_den
-        num2 = other.num
-        den2 = other.den
+        num = self.__num
+        den = self.__den
+        num2 = other.get_num
+        den2 = other.get_den
         num_result = num * den2 + num2 * den
         den_result = den * den2
         return Fraction(num_result, den_result)
@@ -131,7 +140,7 @@ class Fraction:
         """Overloading of the - operator for Fractions
 
         PRE : No specific preconditions
-        POST : returns a new Fraction class instance representing the result of the subtraction
+        POST : Returns a new Fraction class instance representing the result of the subtraction
         RAISE : NotFractionInstanceException if "other" argument is not a Fraction class instance
         """
         if not(isinstance(other, Fraction)):
@@ -139,8 +148,8 @@ class Fraction:
 
         num = self.__num
         den = self.__den
-        num2 = other.num
-        den2 = other.den
+        num2 = other.get_num
+        den2 = other.get_den
         num_result = num * den2 - num2 * den
         den_result = den * den2
         return Fraction(num_result, den_result)
@@ -149,7 +158,7 @@ class Fraction:
         """Overloading of the * operator for fractions
 
         PRE : No specific preconditions
-        POST : returns a new Fraction class instance representing the result of the multiplication
+        POST : Returns a new Fraction class instance representing the result of the multiplication
         RAISE : NotFractionInstanceException if "other" argument is not a Fraction class instance
         """
         if not(isinstance(other, Fraction)):
@@ -157,8 +166,8 @@ class Fraction:
 
         num = self.__num
         den = self.__den
-        num2 = other.num
-        den2 = other.den
+        num2 = other.get_num
+        den2 = other.get_den
         num_result = num * num2
         den_result = den * den2
 
@@ -168,7 +177,7 @@ class Fraction:
         """Overloading of the / operator for fractions
 
         PRE : No specific preconditions
-        POST : returns a Fraction class instance representing the result of the division
+        POST : Returns a Fraction class instance representing the result of the division
         RAISE : NotFractionInstanceException if "other" argument is not a Fraction class instance
         """
         if not(isinstance(other, Fraction)):
@@ -176,8 +185,8 @@ class Fraction:
 
         num = self.__num
         den = self.__den
-        num2 = other.num
-        den2 = other.den
+        num2 = other.get_num
+        den2 = other.get_den
 
         num_result = num * den2
         den_result = den * num2
@@ -197,8 +206,8 @@ class Fraction:
 
         num = self.__num
         den = self.__den
-        num2 = other.num
-        den2 = other.den
+        num2 = other.get_num
+        den2 = other.get_den
 
         num_result = num**(num2/den2)
         den_result = den**(num2/den2)
@@ -217,8 +226,8 @@ class Fraction:
 
         num = self.__num
         den = self.__den
-        num2 = other.__num
-        den2 = other.__den
+        num2 = other.get_num
+        den2 = other.get_den
         return num * den2 == num2 * den
 
     def __float__(self):
@@ -228,20 +237,21 @@ class Fraction:
         POST : Returns the decimal value of the fraction
         """
 
-        return self.num / self.den
+        return self.__num / self.__den
 
     # TODO : [BONUS] You can overload other operators if you wish (ex : <, >, ...)
 
 
     # ------------------ Properties checking  ------------------
 
+
     def is_zero(self):
         """Check if a fraction's value is 0
 
         PRE : No specific preconditions
-        POST : Returns True if num / den == 0 else returns False
+        POST : Returns True if num == 0 else returns False
         """
-        return self.num == 0
+        return self.__num == 0
 
     def is_integer(self):
         """Check if a fraction is integer (ex : 8/4, 3, 2/2, ...)
@@ -249,8 +259,8 @@ class Fraction:
         PRE : No specific preconditions
         POST : return True if the fraction is a round number, return False if not
         """
-        num = self.__simplified_num
-        den = self.__simplified_den
+        num = self.__num
+        den = self.__den
 
         return num / den % 1 == 0
 
@@ -260,8 +270,8 @@ class Fraction:
         PRE : No specific preconditions
         POST : return True if the absolute value of the fraction is < 1 return False if not
         """
-        num = abs(self.__simplified_num)
-        den = abs(self.__simplified_den)
+        num = abs(self.__num)
+        den = abs(self.__den)
 
         return num < den
 
@@ -271,7 +281,7 @@ class Fraction:
         PRE : No specific preconditions
         POST : return True if the simplified numerator is == 1 if not return False
         """
-        return self.__simplified_num == 1
+        return self.__num == 1
 
     def is_adjacent_to(self, other):
         """Check if two fractions differ by a unit fraction
@@ -285,6 +295,6 @@ class Fraction:
         if not(isinstance(other, Fraction)):
             raise NotFractionInstanceException("The operation must be between two Fraction class instances")
 
-        result_frac = Fraction(abs(self.__simplified_num), abs(self.__simplified_den)) - Fraction(abs(other.__simplified_num), abs(other.__simplified_den))
+        result_frac = Fraction(abs(self.__num), abs(self.__den)) - Fraction(abs(other.get_num), abs(other.get_den))
 
         return result_frac.is_unit()
